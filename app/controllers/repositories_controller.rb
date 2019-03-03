39c639fa59ab6a933e3a76c40ea8a2c70bcc8066
Taylor Williams <taylor@thewilliams.net>
2019-03-03 21:23:23 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,12 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = Iv1.7d3ec104ca1b1d78
       req.params['client_secret'] = b312059fc109dbfb2487d57e47c07a5ac2f81809
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
+      req.params['q'] = params[:search]
     end
     render 'search'
   end
